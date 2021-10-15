954a25dbedfe12ea7fdb271e9c6460074e6745a1
Andrew Ahn <wow01022634@gmail.com>
2021-10-14 19:35:20 -0700

    Add files via upload

---
+++ sesion_2.js
@@ -0,0 +1,72 @@
+﻿function operatorStudy() {
+    //basic operators
+    var a = 1;
+    var b = 2;
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+}
+
+function conditionalLogic() {
+
+    //If statement
+    var x = 3;
+    //equal
+    if (x == 2)
+     { console.log("x is 2"); }
+    //not equal
+    if (x != 2)
+      { console.log("x is not 2"); }
+    //bigger than
+    if (x > 2) { console.log("x is bigger than 2"); }
+    //less than
+    if (x < 2) { console.log("x is less than 2"); }
+
+
+    //Using If Else Statement
+    var y = 3;
+    if (y == 2) {
+        console.log('y  is 2')
+    } else if (y  != 2) {
+        console.log('y  is not 2')
+    } else if (y  < 2) {
+        console.log('y is less  than 2')
+    } else {
+        console.log('y is bigger than 2')
+    }
+}
+
+function ArrayStudy1() {
+    //1 Dimentional Array
+    var wallet = ['visa', 'mastercard', 'costo']
+    console.log(wallet[0]);
+
+    //1 Dimentional Arry Looping
+    var i = 0;
+    while (i < dept.length) {
+        console.log(wallet[i]);
+        i = i + 1;
+    }
+    //Iterates till calculated condition is met   
+    for (let i = 0; i < wallet.length; i++)
+    {
+        console.log(wallet[i]);
+    }
+}
+
+function ArrayStudy2() {
+    //2 Dimensional Array Definition
+    var wallet = [
+        ['visa', 'Citi'],
+        ['Amex', 'Delta'],
+        ['Mastercard', 'USBank'],
+    ];
+    //2 Dimensional Array Looping
+    for (var i = 0; i < wallet.length; i++) {
+        var cube = wallet[i];
+        for (var j = 0; j < cube.length; j++) {
+            console.log("cube[" + i + "][" + j + "] = " + cube[j]);
+        }
+    }
+}
---
+++ session_1.js
@@ -0,0 +1,26 @@
+﻿function myFunction() {
+
+    console.log("hello World")
+}
+//basic output out of function by passing x parameter
+function salesScore(x) {
+    var sscore = x * 20;
+    return sscore;
+}
+//error if you don’t pass x,y as both of them are used in the function
+function salesScore(x, y) {
+    var sscore = x * y * 20;
+    return sscore;
+}
+
+//no error if you don’t pass y but error if you don't pass x as x is used in the function
+function salesScore(x, y) {
+    var sscore = x * 20;
+    return sscore;
+}
+
+//how to avoid error you can provide default value in the parameter where you don't have to pass parameter even though it's is used
+function salesScore(x = 0, y = 0) {
+    var sscore = x * y * 20;
+    return sscore;
+}
